d00e4e6086c6d70ec549bbb99f8a32f5e8254183
Jared Tong <tongrhj@users.noreply.github.com>
2018-07-19 00:57:22 +0800

    Start tracking when venue is first added

--- scripts/parseVenueJson.js
+++ scripts/parseVenueJson.js
@@ -3,12 +3,18 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('historical_data/venues_18July2018.json'));
 const existingData = JSON.parse(fs.readFileSync('historical_data/venues_16July2018.min.json'));
+
+const withinPastWeek = function(ms) {
+  return ms >= Date.now() - 604800000
+}
+
 const newData = data.data.map(function(d){
   const newD = {
     id: d.id,
     name: d.name,
     formatted_price: d.formatted_price,
-    newly_added: !existingData.find(function(venue){return venue.id == d.id}),
+    newly_added: !existingData.find(function(venue){return venue.id == d.id}) || withinPastWeek(d.time_first_added),
+    time_first_added: !existingData.find(function(venue){return venue.id == d.id}) ? Date.now() : Date.now() - 1728000000,
     removed: false,
   };
   return newD;
