fc2c2350295612029cddb6200290a77e15d54b89
Jared Tong <tongrhj@users.noreply.github.com>
2018-07-27 00:03:45 +0800

    Update for 27 July Data

--- scripts/updateLastUpdatedDate.js
+++ scripts/updateLastUpdatedDate.js
@@ -6,12 +6,12 @@ const replace = require('replace-in-file');
 const options = {
   files: './dist/index.html',
   from: /<span id="last-updated-date">.*<\/span>/,
-  to: '<span id="last-updated-date">23 July 2018</span>',
+  to: '<span id="last-updated-date">27 July 2018</span>',
 };
 
 replace(options, (error, changes) => {
   if (error) {
     return console.error('Error occurred:', error);
   }
-  console.log('Modified files:', changes.join(', '));
+  console.log('Updated date for files:', changes.join(', '));
 });
