5f44933b6d1ae6d64870c3c3d8ab3dbcb6055186
Jared Tong <tongrhj@users.noreply.github.com>
2018-09-25 17:10:04 +0800

    Update frontend to prevent resize

--- scripts/parseVenueJson.js
+++ scripts/parseVenueJson.js
@@ -2,20 +2,17 @@
 
 const fs = require('fs');
 const got = require('got');
-
 const notifyTelegram = require('./notifyTelegram').default;
 
 (async () => {
 
 const latestDataResponse = await got(process.env.VENUE_URL, { json: true, headers: { 'user-agent': null } })
 const latestData = latestDataResponse.body
+// {meta: {"code":200}, data: [{"id":164979,"name":"5 Little Monkeys Cafe","formatted_price":"~$15/pax","beyond_partner":true,"beyond_newly_added":true,"open_now":true,"location":{"city":"Singapore","country":"Singapore","state":"","street":"20 Kallang Avenue","address_2":"#11-00 Pico Creative Centre","zipcode":"339411","latitude":1.3098178,"longitude":103.8650975,"neighbourhood":"Kallang"},"avatar":{"url":"https://s3.burpple.com/venues/cafe_logo_-cropped-_copy-png_164979_original?1537238727","medium_url":"https://s3.burpple.com/venues/cafe_logo_-cropped-_copy-png_164979_medium?1537238727","small_url":"https://s3.burpple.com/venues/cafe_logo_-cropped-_copy-png_164979_small?1537238727"}}}
 
 const existingDataResponse = await got(process.env.PARSED_VENUE_URL, { json: true })
 const existingData = existingDataResponse.body
-
-const withinPastWeek = function(ms) {
-  return ms >= Date.now() - 604800000
-}
+// [{"id":164979,"name":"5 Little Monkeys Cafe","formatted_price":"~$15/pax","newly_added":true,"time_first_added":1537864234970,"removed":false,"location":{"longitude":103.8650975,"latitude":1.3098178,"neighbourhood":"Kallang"}}]
 
 const newData = latestData.data.map(function(d){
   // Returns first matching Venue Object or undefined
