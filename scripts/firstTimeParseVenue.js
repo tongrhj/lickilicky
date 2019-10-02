48e25bfb06afefca7e459db9b77be54409602b9c
Jared Tong <tongrhj@users.noreply.github.com>
2019-10-02 23:02:57 +1000

    Avoid posting to telegram

--- scripts/firstTimeParseVenue.js
+++ scripts/firstTimeParseVenue.js
@@ -249,7 +249,7 @@ Array.prototype.contains = function(obj) {
       formatData(venuesReturningSinceLastRun),
       formatData(venuesExpiring),
       formatData(venuesWithDealsChanged, { includePreviousDeals: true }),
-      { chat_ids: [TELEGRAM_CHAT_ID, TELEGRAM_CHAT_ID_2] }
+      { chat_ids: [] }
     );
   } catch (e) {
     console.error(e);
